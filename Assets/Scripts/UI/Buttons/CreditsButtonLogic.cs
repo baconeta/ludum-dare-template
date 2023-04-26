d28d25b78e75db50b192b09e710de524ee9f78e8
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-04-26 22:30:20 +0300

    Add some specific, non generic analytics as examples

---
+++ Assets/Scripts/UI/Buttons/CreditsButtonLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Unity.Services.Analytics;
+using UnityEngine;
+
+public class CreditsButtonLogic : MonoBehaviour
+{
+    private float _cachedTimeCreditsOpen;
+
+    public void PressCreditsButton()
+    {
+        _cachedTimeCreditsOpen = Time.time;
+
+        // The ‘creditsViewed’ event will get cached locally 
+        //and sent during the next scheduled upload, within 1 minute
+        AnalyticsService.Instance.CustomData("creditsViewed");
+
+        // You can call Events.Flush() to send the event immediately
+        AnalyticsService.Instance.Flush();
+    }
+
+    public void PressBackButton()
+    {
+        var currentTime = Time.time;
+        var totalTimeOnScreen = currentTime - _cachedTimeCreditsOpen;
+
+        //Define Custom Parameters
+        Dictionary<string, object> parameters = new Dictionary<string, object>()
+        {
+            {"timeOnCreditsScreen", totalTimeOnScreen.ToString(CultureInfo.InvariantCulture)}
+        };
+
+        AnalyticsService.Instance.CustomData("creditsClosed", parameters);
+        AnalyticsService.Instance.Flush();
+    }
+}
