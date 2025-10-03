54d8c31d49bce3c6e3e8f4d5b6c92b7ed45f03d8
Josh Villgren <joshuavillgren@gmail.com>
2025-10-03 14:54:30 +1300

    add remaining utils

---
+++ Assets/Scripts/Utils/LightFlicker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+    public float minIntensity = 1f;
+    public float maxIntensity = 3f;
+    public float flickerSpeed = 0.1f;
+
+    private Light lightSource;
+
+    private void Start()
+    {
+        lightSource = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        // Smooth flicker using Perlin noise (natural feel)
+        var noise = Mathf.PerlinNoise(Time.time * flickerSpeed, 0.0f);
+        lightSource.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+}
